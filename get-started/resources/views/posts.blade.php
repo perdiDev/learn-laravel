95c206a3eb9b995918eba7f16b44155c44eb8a75
PerdiDev <perdidev@gmail.com>
2023-02-06 20:05:18 +0800

    Laravel 4: templating engine (blade)

--- get-started/resources/views/posts.blade.php
+++ get-started/resources/views/posts.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perdi Blog | Blog</title>
-</head>
-<body>
-    <h1>Halaman Blog</h1>
-</body>
-</html>
+
+@extends('layouts.main')
+
+@section('container')
+
+    @foreach ($posts as $post)
+        <h3>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a></h3>
+        <h5>{{ $post["author"] }}</h5>
+        <p>{{ $post["body"] }}</p>
+    @endforeach
+
+@endsection
--- get-started/routes/web.php
+++ get-started/routes/web.php
@@ -14,16 +14,65 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Perdi",
         "pendidikan" => "UNHAS",
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    
+    $blog_posts = [
+        [
+            "title" => "Blog Pertama",
+            "slug" => "blog-pertama",
+            "author" => "Perdi",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse reiciendis dicta, facilis sint, laboriosam ullam, veritatis minima autem amet debitis ex omnis mollitia. Eaque veniam adipisci aliquid ducimus assumenda enim maiores, nostrum quos! Veritatis sapiente totam vel quam ab qui laborum repudiandae neque labore ullam doloribus expedita, corporis perferendis tenetur maiores atque minus quis assumenda quod ea earum? Fugiat in reiciendis inventore iure cupiditate explicabo dolorum quia excepturi ullam! Laboriosam reprehenderit nesciunt recusandae ullam, totam dolorem quisquam iure aperiam eius, eligendi doloremque ipsum nulla omnis ducimus at. Commodi, provident rerum."
+        ],
+        [
+            "title" => "Blog Kedua",
+            "slug" => "blog-kedua",
+            "author" => "Perdi",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, similique quia, est voluptas doloribus minima, inventore iste perspiciatis velit natus neque laudantium tenetur rem magnam itaque fuga sint possimus beatae?"
+        ],
+    ];
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => $blog_posts,
+    ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Blog Pertama",
+            "slug" => "blog-pertama",
+            "author" => "Perdi",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse reiciendis dicta, facilis sint, laboriosam ullam, veritatis minima autem amet debitis ex omnis mollitia. Eaque veniam adipisci aliquid ducimus assumenda enim maiores, nostrum quos! Veritatis sapiente totam vel quam ab qui laborum repudiandae neque labore ullam doloribus expedita, corporis perferendis tenetur maiores atque minus quis assumenda quod ea earum? Fugiat in reiciendis inventore iure cupiditate explicabo dolorum quia excepturi ullam! Laboriosam reprehenderit nesciunt recusandae ullam, totam dolorem quisquam iure aperiam eius, eligendi doloremque ipsum nulla omnis ducimus at. Commodi, provident rerum."
+        ],
+        [
+            "title" => "Blog Kedua",
+            "slug" => "blog-kedua",
+            "author" => "Perdi",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, similique quia, est voluptas doloribus minima, inventore iste perspiciatis velit natus neque laudantium tenetur rem magnam itaque fuga sint possimus beatae?"
+        ],
+    ];
+
+    $post_open = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $post_open = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $post_open,
+    ]);
 });
